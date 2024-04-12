5adcfcefca0eee31648efc425f6d7e1054d58760
NguyenTam03 <locnam0312@gmail.com>
2024-04-12 15:04:51 +0700

    update

--- src/main/java/com/webspringmvc/entity/KhuyenMai.java
+++ src/main/java/com/webspringmvc/entity/KhuyenMai.java
@@ -1,5 +1,60 @@
 package com.webspringmvc.entity;
 
-public class KhuyenMai {
+import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "KHUYENMAI")
+public class KhuyenMai {
+	@Id
+	private String MaKM;
+	private String TenKM;
+	private Date NgayBD;
+	private Date NgayKT;
+	
+	public KhuyenMai() {
+	}
+
+	public KhuyenMai(String maKM, String tenKM, Date ngayBD, Date ngayKT) {
+		MaKM = maKM;
+		TenKM = tenKM;
+		NgayBD = ngayBD;
+		NgayKT = ngayKT;
+	}
+
+	public String getMaKM() {
+		return MaKM;
+	}
+
+	public void setMaKM(String maKM) {
+		MaKM = maKM;
+	}
+
+	public String getTenKM() {
+		return TenKM;
+	}
+
+	public void setTenKM(String tenKM) {
+		TenKM = tenKM;
+	}
+
+	public Date getNgayBD() {
+		return NgayBD;
+	}
+
+	public void setNgayBD(Date ngayBD) {
+		NgayBD = ngayBD;
+	}
+
+	public Date getNgayKT() {
+		return NgayKT;
+	}
+
+	public void setNgayKT(Date ngayKT) {
+		NgayKT = ngayKT;
+	}
+	
 }
